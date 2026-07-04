3bf43b69c98dd1f24f3af523817a1beae97e5254
navya-78 <devanaboinanavya7@gmail.com>
2026-07-04 13:13:05 +0530

    added Navya naveen

--- src/test/java/com/neoteric/Hello.java
+++ src/test/java/com/neoteric/Hello.java
@@ -2,5 +2,6 @@ package com.neoteric;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Navya Naveen");
     }
 }
